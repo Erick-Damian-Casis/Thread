3b60c024063a6d8a182f90c282149b921ba71354
ERICK-DC <67754832+ERICK-DC@users.noreply.github.com>
2021-01-17 22:59:04 -0500

    Thread

---
+++ Hilos/Carrera.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace Hilos
+{
+    class Carrera 
+    {
+        public static void Corredor(object obj) 
+        {
+            var nombre = (string)obj;
+            var pasos = 0;
+            var random = new Random();
+            while (pasos < 10000) 
+            {
+                pasos += random.Next(0,300);
+                Console.WriteLine("Corredor {0} va a {1} km/h", nombre, pasos);
+                Thread.Sleep(10);
+            }
+            Console.WriteLine("{0} a llego a la meta. ************Felicidades!***************", nombre);
+        }
+    }
+}
---
+++ Hilos/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+
+namespace Hilos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Thread corredor1 = new Thread(Carrera.Corredor);
+            Thread corredor2 = new Thread(Carrera.Corredor);
+            Thread corredor3 = new Thread(Carrera.Corredor);
+            Thread corredor4 = new Thread(Carrera.Corredor);
+
+            corredor1.Start("Ferrari");
+            corredor2.Start("Lamborghini");
+            corredor3.Start("Renault");
+            corredor4.Start("Maserati");
+
+            corredor1.Join();
+            corredor2.Join();
+            corredor3.Join();
+            corredor4.Join();
+            
+         
+        }
+
+        static void MetodoPorEjecutar()
+        {
+            var hiloActual = Thread.CurrentThread;
+            hiloActual.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+            Console.WriteLine("Hilo actual {0}: ",hiloActual.ManagedThreadId);
+            Console.WriteLine("Mi cultura es {0}: ", hiloActual.CurrentCulture);
+
+            var random = new Random();
+
+            for (int i =0; i <= 10; i++) 
+            {
+                Console.WriteLine("hilo {0} indice {1}", hiloActual.ManagedThreadId, i);
+                Thread.Sleep(random.Next(100,500));
+            }
+
+           
+        }
+
+    }
+}
